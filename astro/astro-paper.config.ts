13ec2a822f6631dac4726bf49ffb7d01eb437691
vishwask1 <96179813+vishwask1@users.noreply.github.com>
2026-09-02 17:23:36 +0000

    og image & about edit

--- astro/astro-paper.config.ts
+++ astro/astro-paper.config.ts
@@ -26,7 +26,7 @@ export default defineAstroPaperConfig({
   },
   features: {
     lightAndDarkMode: true,
-    dynamicOgImage: true,
+    dynamicOgImage: false,
     showArchives: true,
     showBackButton: true,
     editPost: {
